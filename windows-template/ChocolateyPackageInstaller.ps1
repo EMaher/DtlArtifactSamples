890bac1ff88b94775266bbc037fda4fde315210d
Elizabeth Maher (NEWMAN) <enewman@microsoft.com>
2018-04-09 20:11:18 -0700

    removing trap statemetn

--- windows-template/ChocolateyPackageInstaller.ps1
+++ windows-template/ChocolateyPackageInstaller.ps1
@@ -27,30 +27,6 @@ $choco = "$Env:ProgramData/chocolatey/choco.exe"
 
 ###################################################################################################
 #
-# Handle all errors in this script.
-#
-
-trap
-{
-    # NOTE: This trap will handle all errors. There should be no need to use a catch below in this
-    #       script, unless you want to ignore a specific error.
-    $message = $Error[0].Exception.Message
-    if ($message)
-    {
-        Write-Host -Object "`nERROR: $message" -ForegroundColor Red
-    }
-
-    Write-Host "`nThe artifact failed to apply.`n"
-
-    # IMPORTANT NOTE: Throwing a terminating error (using $ErrorActionPreference = "Stop") still
-    # returns exit code zero from the PowerShell script when using -File. The workaround is to
-    # NOT use -File when calling this script and leverage the try-catch-finally block and return
-    # a non-zero exit code from the catch block.
-    exit -1
-}
-
-###################################################################################################
-#
 # Functions used in this script.
 #
 
@@ -156,7 +132,6 @@ try
     Write-Host "Preparing to install Chocolatey packages: $Packages."
     Install-Packages -Packages $Packages
 
-    Write-Host "`nThe artifact was applied successfully.`n"
 }
 finally
 {
